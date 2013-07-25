14a68189bf7abe1268e08f738e29fb929def293e
Andy Lawrence <andy.lawrence@gmail.com>
2013-07-25 10:35:22 -0400

    adding a second test

--- test/mocha_tests.js
+++ test/mocha_tests.js
@@ -1,5 +1,6 @@
 var request = require('superagent'); 
 var expect = require('expect.js');
+var assert = require("assert")
 
 
 describe('basic stuff', function(){ 
@@ -14,3 +15,11 @@ describe('basic stuff', function(){
     }); 
 }); 
     
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
